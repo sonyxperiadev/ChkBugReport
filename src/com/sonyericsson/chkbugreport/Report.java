f884707a2dd0eb8f7aeb71e06b3b4c465de1b217
Pal Szasz <pal.szasz@sonymobile.com>
2012-08-22 15:00:04 +0200

    Increased version number

--- src/com/sonyericsson/chkbugreport/Report.java
+++ src/com/sonyericsson/chkbugreport/Report.java
@@ -35,7 +35,7 @@ import java.util.Vector;
 public abstract class Report {
 
     public static final String VERSION = "0.3";
-    public static final String VERSION_CODE = "130";
+    public static final String VERSION_CODE = "131";
 
     private String mFileName;
     private String mOutDir;
