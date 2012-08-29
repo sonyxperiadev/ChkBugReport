281442d197a018c4ba5ff5ee090c14949b38e857
Pal Szasz <pal.szasz@sonymobile.com>
2012-08-29 08:51:18 +0200

    Added a new type of graph for _sample events
    Also did minor refactoring

--- src/com/sonyericsson/chkbugreport/plugins/logs/event/EventLogPlugin.java
+++ src/com/sonyericsson/chkbugreport/plugins/logs/event/EventLogPlugin.java
@@ -27,21 +27,8 @@ import com.sonyericsson.chkbugreport.Util;
 import com.sonyericsson.chkbugreport.plugins.logs.LogLine;
 import com.sonyericsson.chkbugreport.plugins.logs.LogPlugin;
 
-import java.awt.Color;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Vector;
-
-import javax.imageio.ImageIO;
 
 public class EventLogPlugin extends LogPlugin {
 
@@ -65,7 +52,7 @@ public class EventLogPlugin extends LogPlugin {
     /* content_query_sample + content_update_stats */
     private HashMap<String, DBStat> mCTStats = new HashMap<String, DBStat>();
     /* The collection of *_sample data */
-    private HashMap<String, Vector<SampleData>> mSDs = new HashMap<String, Vector<SampleData>>();
+    private SampleDatas mSDs;
 
     private ActivityManagerTrace mAM;
 
@@ -86,7 +73,7 @@ public class EventLogPlugin extends LogPlugin {
         mCQStats.clear();
         mCUStats.clear();
         mCTStats.clear();
-        mSDs.clear();
+        mSDs = new SampleDatas();
         mAM = new ActivityManagerTrace(this, br);
         super.load(br);
         mAM.finishLoad();
@@ -102,7 +89,7 @@ public class EventLogPlugin extends LogPlugin {
         // Finish sub-chapters
         finishActivityLaunchTime(br, ch);
         finishDBStats(br, ch);
-        generateSampleDataGraphs(br, ch);
+        new SampleDatasGenerator(this, mSDs).run(br, ch);
         new ActivityManagerGraphGenerator(this, mAM).run(br, ch);
         new ActivityManagerStatsGenerator(this, mAM).run(br, ch);
         new ActivityManagerProcStatsGenerator(this, mAM).run(br, ch);
@@ -365,12 +352,7 @@ public class EventLogPlugin extends LogPlugin {
     }
 
     private void addSampleData(String eventType, SampleData sd) {
-        Vector<SampleData> sds = mSDs.get(eventType);
-        if (sds == null) {
-            sds = new Vector<SampleData>();
-            mSDs.put(eventType, sds);
-        }
-        sds.add(sd);
+        mSDs.addData(eventType, sd);
     }
 
     private void addDvmLockSampleDataC(Report br, String eventType, LogLine sl) {
@@ -502,7 +484,7 @@ public class EventLogPlugin extends LogPlugin {
     }
 
     private void collectSampleStats(String eventType, HashMap<String, DBStat> stats) {
-        Vector<SampleData> datas = mSDs.get(eventType);
+        Vector<SampleData> datas = mSDs.getSamplesByType(eventType);
         if (datas == null) return;
         for (SampleData sd : datas) {
             addDBData(sd.pid, sd.name, sd.duration, stats);
@@ -582,246 +564,5 @@ public class EventLogPlugin extends LogPlugin {
         }
         return cnt;
     }
-
-    private void generateSampleDataGraphs(Report br, Chapter mainCh) {
-        Chapter ch = null;
-
-        for (Entry<String, Vector<SampleData>> entry : mSDs.entrySet()) {
-            String eventType = entry.getKey();
-            Vector<SampleData> sds = entry.getValue();
-            if (sds.size() <= 1) continue;
-
-            // Create the chapter if not created yet
-            if (ch == null) {
-                ch = new Chapter(br, "*_sample graphs");
-                mainCh.addChapter(ch);
-            }
-
-            // create the graph
-            ch.addLine("<p>Graph built from " + eventType + " logs:</p>");
-            // save the data as vcd file as well
-            String fnVcd = br.getRelRawDir() + "sample_graph_" + eventType + ".vcd";
-            if (generateSampleDataVCD(br, fnVcd, sds, eventType)) {
-                // TODO
-            }
-            ch.addLine("<div class=\"hint\">(VCD file also generated: <a href=\"" + fnVcd + "\">" + fnVcd + "</a>)</div>");
-            String fn = br.getRelDataDir() + "sample_graph_" + eventType + ".png";
-            if (generateSampleDataGraph(br, fn, sds, eventType)) {
-                // TODO
-            }
-            ch.addLine("<div><img src=\"" + fn + "\"/></div>");
-
-        }
-    }
-
-    private boolean generateSampleDataGraph(Report br, String fn, Vector<SampleData> sds, String eventType) {
-        int w = 800;
-        int h = 350;
-        int cx = 100;
-        int cy = 250;
-        int gw = 600;
-        int gh = 200;
-        int nx = 750;
-        int ny = 10;
-        int tx = cx;
-        int ty = cy;
-        int th = 75;
-
-        int maxNameW = 0;
-
-        // Need a font metrics before the actual image is created :-(
-        FontMetrics fm = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB).getGraphics().getFontMetrics();
-
-        // Allocate colors for each value
-        // Also count the number of distinct values
-        HashMap<String, Color> mColors = new HashMap<String, Color>();
-        int idx = 0, lh = fm.getHeight();
-        if (lh < 18) {
-            lh = 18;
-        }
-        for (SampleData sd : sds) {
-            String name = sd.name;
-            Color col = mColors.get(name);
-            if (col == null) {
-                int rgba = Util.getColor(idx++) | 0x40000000;
-                col = new Color(rgba, true);
-                mColors.put(name, col);
-            }
-            maxNameW = Math.max(maxNameW, fm.stringWidth(name));
-        }
-        w += maxNameW + 32;
-        int maxNameH = ny * 2 + idx * lh;
-        h = Math.max(h, maxNameH);
-
-        // Create an empty image
-        BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = (Graphics2D)img.getGraphics();
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, w, h);
-        g.setColor(Color.LIGHT_GRAY);
-        g.drawRect(0, 0, w - 1, h - 1);
-
-        // Draw the legend
-        for (Entry<String, Color> entry : mColors.entrySet()) {
-            String name = entry.getKey();
-            Color color = entry.getValue();
-            g.setColor(color);
-            g.fillRect(nx, ny + (lh - 16) / 2, 16, 16);
-            g.drawRect(nx, ny + (lh - 16) / 2, 15, 15);
-            g.setColor(Color.BLACK);
-            g.drawString(name, nx + 32, ny + fm.getAscent());
-            ny += lh;
-        }
-
-        // Draw the axis
-        int as = 5;
-        g.setColor(Color.BLACK);
-        g.drawLine(cx, cy, cx, cy - gh);
-        g.drawLine(cx, cy, cx + gw, cy);
-        g.drawLine(cx - as, cy - gh + as, cx, cy - gh);
-        g.drawLine(cx + as, cy - gh + as, cx, cy - gh);
-        g.drawLine(cx + gw - as, cy - as, cx + gw, cy);
-        g.drawLine(cx + gw - as, cy + as, cx + gw, cy);
-
-        // Draw the title
-        g.drawString(eventType, 10, 10 + fm.getAscent());
-
-        // Draw some guide lines
-        int max = 110;
-        int count = 5;
-        int step = 20;
-        Color colGuide = new Color(0xc0c0ff);
-        for (int i = 1; i <= count; i++) {
-            int value = i * step;
-            if (value > max) break;
-            int yv = cy - value * gh / max;
-            g.setColor(colGuide);
-            g.drawLine(cx + 1, yv, cx + gw, yv);
-            g.setColor(Color.BLACK);
-            String s = "" + value + "%  ";
-            g.drawString(s, cx - fm.stringWidth(s) - 1, yv);
-        }
-
-        // Plot the values (size)
-        long duration = (getLastTs() - getFirstTs());
-        if (duration <= 0) return false;
-        for (SampleData sd : sds) {
-            int bh = sd.perc * (gh - 1) / max;
-            int bx = (int)((sd.ts - getFirstTs()) * (gw - 1) / duration);
-            int bw = (int)((sd.duration) * (gw - 1) / (getLastTs() - getFirstTs()));
-            if (bw < 3) {
-                bw = 3;
-            }
-            g.setColor(mColors.get(sd.name));
-            g.fillRect(cx + bx - bw, cy - bh, bw, bh);
-            g.drawRect(cx + bx - bw, cy - bh, bw - 1, bh - 1);
-        }
-
-        // Draw the time line
-        if (!Util.renderTimeBar(img, g, tx, ty, gw, th, getFirstTs(), getLastTs(), true)) {
-            return false;
-        }
-
-        // Save the image
-        try {
-            ImageIO.write(img, "png", new File(br.getBaseDir() + fn));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean generateSampleDataVCD(Report br, String fn, Vector<SampleData> sds, String eventType) {
-        if (sds.size() == 0) return false;
-
-        // In the first pass we need to find the unique ids, and also generate a sorted
-        // list of events
-        HashMap<String, Integer> ids = new HashMap<String, Integer>();
-        Vector<SampleEvent> events = new Vector<SampleEvent>();
-        int idx = 0;
-        for (SampleData sd : sds) {
-            String name = sd.name;
-
-            // Associate an id to the name
-            Integer idObj = ids.get(name);
-            if (idObj == null) {
-                idObj = new Integer(idx++);
-                ids.put(name, idObj);
-            }
-
-            // Add the start and stop events
-            events.add(new SampleEvent(true, sd.ts - sd.duration, idObj));
-            events.add(new SampleEvent(false, sd.ts, idObj));
-        }
-
-        // Sort the events by timestamp
-        Collections.sort(events, new Comparator<SampleEvent>() {
-            @Override
-            public int compare(SampleEvent o1, SampleEvent o2) {
-                if (o1.ts < o2.ts) return -1;
-                if (o1.ts > o2.ts) return +1;
-                return 0;
-            }
-        });
-
-        // Save the file
-        try {
-            PrintStream ps = new PrintStream(new File(br.getBaseDir() + fn));
-            int bits = 8;
-
-            // Write header
-            ps.println("$timescale 1ms $end");
-            ps.println("$scope am_logs $end");
-            for (Entry<String, Integer> entry : ids.entrySet()) {
-                String name = entry.getKey();
-                int id = entry.getValue();
-                name = Util.fixVCDName(name);
-                ps.println("$var wire " + bits + " n" + id + " " + name + " $end");
-            }
-            ps.println("$upscope $end");
-            ps.println("$enddefinitions $end");
-
-            // Write initial values
-            long lastTs = getFirstTs();
-            ps.println("#" + lastTs);
-            for (int id = 0; id < idx; id++) {
-                ps.println("bZ n" + id);
-            }
-
-            // Write events
-            int count[] = new int[idx];
-            for (SampleEvent event : events) {
-                if (event.ts != lastTs) {
-                    ps.println("#" + event.ts);
-                    lastTs = event.ts;
-                }
-                int id = event.id;
-                if (event.start) {
-                    count[id]++;
-                } else {
-                    count[id]--;
-                }
-                ps.println("b" + (count[id] == 0 ? "Z" : Util.toBinary(count[id], bits)) + " n" + id);
-            }
-
-            // Write final values
-            lastTs = getLastTs();
-            ps.println("#" + lastTs);
-            for (int id = 0; id < idx; id++) {
-                ps.println("bZ n" + id);
-            }
-
-            // Finish
-            ps.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
 
 }
