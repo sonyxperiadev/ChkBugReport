8f3863f43c8922a9b09aff95e5f00ea13dbb37a1
Pal Szasz <pal.szasz@sonymobile.com>
2012-10-03 11:05:59 +0200

    Increased version number

--- src/com/sonyericsson/chkbugreport/Module.java
+++ src/com/sonyericsson/chkbugreport/Module.java
@@ -48,7 +48,7 @@ import java.util.jar.Manifest;
 public abstract class Module {
 
     public static final String VERSION = "0.4";
-    public static final String VERSION_CODE = "164";
+    public static final String VERSION_CODE = "165";
 
     /** The list of installed plugins */
     private Vector<Plugin> mPlugins = new Vector<Plugin>();
