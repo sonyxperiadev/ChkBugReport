e49239ef5b4828345300bb67173b18ecf0a860c5
Pal Szasz <pal.szasz@sonymobile.com>
2012-11-08 15:49:58 +0100

    Increased version number

--- src/com/sonyericsson/chkbugreport/Module.java
+++ src/com/sonyericsson/chkbugreport/Module.java
@@ -50,7 +50,7 @@ import java.util.jar.Manifest;
 public abstract class Module implements ChapterParent {
 
     public static final String VERSION = "0.4";
-    public static final String VERSION_CODE = "176";
+    public static final String VERSION_CODE = "177";
 
     /** Contains some global configuration which could affect the module/plugins behavior */
     private Context mContext;
