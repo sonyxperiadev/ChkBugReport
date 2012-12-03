6f7c685c50dca5d2de31745f5c23cffd11da3fa7
Pal Szasz <pal.szasz@sonymobile.com>
2012-12-03 10:06:18 +0100

    Increased version number

--- src/com/sonyericsson/chkbugreport/Module.java
+++ src/com/sonyericsson/chkbugreport/Module.java
@@ -65,7 +65,7 @@ public abstract class Module implements ChapterParent {
      * The incremental release number of the application
      * (this number is always incremented, never reset or decremented)
      */
-    public static final String VERSION_CODE = "184";
+    public static final String VERSION_CODE = "185";
 
     /** The name of the file where the output will be logged */
     private static final String LOG_NAME = "chkbugreport_log.txt";
