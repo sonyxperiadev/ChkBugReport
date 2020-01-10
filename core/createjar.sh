36e597dffa60616676a161d97e12fd0067d67664
Stephen Lau <stephen@lau.engineering>
2020-01-10 15:20:25 +0100

    Add unit testing and clean up build script (#32)
    * Split build into 3 parts
    * Rename to build.xml
    * Remove eclipse comment.
    * Add unit tests with jUnit
    
    Note that theres just one basic test right now, but it checks that we
    have everything we need to write new tests.
    
    Co-Authored-By: toshi38-sony <43984100+toshi38-sony@users.noreply.github.com>

--- core/createjar.sh
+++ core/createjar.sh
@@ -5,6 +5,6 @@ VER=`grep "VERSION = " $SRC | cut -d '"' -f 2`
 REL=`grep "VERSION_CODE = " $SRC | cut -d '"' -f 2` 
 #echo VER=$VER REL=$REL
 
-ant -f createjar.xml || exit 1
+ant -f build.xml || exit 1
 mv chkbugreport.jar chkbugreport-$VER-$REL.jar || exit 1
 
